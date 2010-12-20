25ed10aaed598d41135802446f3c908493714ca7
jenslyn42 <jenslyn42@8b376f4d-d59e-8b93-49d2-cb07f26c9d22>
2010-12-20 06:15:34 +0000

    tests

---
+++ workspace/testFramework/src/testCacheSuite/LRUTest.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+package testCacheSuite;
+
+import junit.framework.TestCase;
+
+/**
+ * @author administrator
+ *
+ */
+public class LRUTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public LRUTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.LRU#LRU(cacheReplacementTest.TestSettings)}.
+	 */
+	public final void testLRU() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.LRU#readQuery(cacheReplacementTest.Pair)}.
+	 */
+	public final void testReadQuery() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.LRU#readQueryList(java.util.ArrayList)}.
+	 */
+	public final void testReadQueryList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.LRU#checkAndUpdateCache(cacheReplacementTest.Pair)}.
+	 */
+	public final void testCheckAndUpdateCache() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.LRU#getCacheHits()}.
+	 */
+	public final void testGetCacheHits() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.LRU#getTotalQueries()}.
+	 */
+	public final void testGetTotalQueries() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ workspace/testFramework/src/testCacheSuite/MinHeapTest.java
@@ -0,0 +1,84 @@
+/**
+ * 
+ */
+package testCacheSuite;
+
+import junit.framework.TestCase;
+
+/**
+ * @author administrator
+ *
+ */
+public class MinHeapTest extends TestCase {
+
+	/**
+	 * @param name
+	 */
+	public MinHeapTest(String name) {
+		super(name);
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	/* (non-Javadoc)
+	 * @see junit.framework.TestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#MinHeap(cacheReplacementTest.Pair, int)}.
+	 */
+	public final void testMinHeap() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#insert(cacheReplacementTest.Pair)}.
+	 */
+	public final void testInsert() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#updateEntry(int, int)}.
+	 */
+	public final void testUpdateEntry() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#isEmpty()}.
+	 */
+	public final void testIsEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#print()}.
+	 */
+	public final void testPrint() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#removeMin()}.
+	 */
+	public final void testRemoveMin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link cacheReplacementTest.MinHeap#peekMin()}.
+	 */
+	public final void testPeekMin() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
