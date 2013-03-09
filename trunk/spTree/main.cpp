894dd85414dccd8fa047ad6bcd963d3ef7d00929
jenslyn42 <jenslyn42@8b376f4d-d59e-8b93-49d2-cb07f26c9d22>
2013-03-09 23:07:55 +0000

    sptree versioned

---
+++ trunk/spTree/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "spTreeRG.h"
+
+using namespace std;
+
+int main()
+{
+
+ //   spTreeRG::mapObject("aalborgNEW")->writeSPtree(spTreeRG::mapObject("aalborgNEW")->readPOIlist());
+    spTreeRG::mapObject("beijingNEW")->writeSPtree(spTreeRG::mapObject("beijingNEW")->readPOIlist());
+
+
+    return 0;
+}
